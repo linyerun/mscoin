c6c185c392a424922ef5a2dfac86e09ab6912c79
随风 <2338244917@qq.com>
2024-05-30 17:30:42 +0800

    feat(tool): 编写工具函数

---
+++ common/tool/okx.go
@@ -0,0 +1,18 @@
+package tool
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+)
+
+//https://www.okx.com/网站提供的API
+
+func ComputeHmacSha256(message string, secret string) string {
+	key := []byte(secret)
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(message))
+	sha := hex.EncodeToString(h.Sum(nil))
+	return base64.StdEncoding.EncodeToString([]byte(sha))
+}
